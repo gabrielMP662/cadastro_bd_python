b3fdb017596cb50a4d5baac6d822c372a5cf7643
gabrielMP662 <muller.gabriel48@yahoo.com>
2025-09-15 15:32:07 -0300

    Primeiro commit

--- projeto_gabriel_bd.py
+++ projeto_gabriel_bd.py
@@ -2,6 +2,14 @@ import sqlite3
 
 banco = sqlite3.connect('primeiro_banco.db')
 
+cursor = banco.cursor()
+cursor.execute('''
+            CREATE TABLE IF NOT EXISTS pessoas (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 nome TEXT NOT NULL,
+                 idade INTEGER NOT NULL,
+                 email TEXT NOT NULL
+                  )  ''')
 while True:
     
     nome = str(input("Qual o seu nome: "))
@@ -13,14 +21,6 @@ while True:
             print('Por favor, digite um numero valido!')
     email = input("qual o seu email? ")
 
-    cursor = banco.cursor()
-    cursor.execute('''
-            CREATE TABLE IF NOT EXISTS pessoas (
-                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                 nome TEXT NOT NULL,
-                 idade INTEGER NOT NULL,
-                 email TEXT NOT NULL
-                  )  ''')
     cursor.execute(f"INSERT INTO pessoas  (nome, idade, email) VALUES (?, ?, ?)", (nome, idade, email))
     banco.commit()
     while True:
